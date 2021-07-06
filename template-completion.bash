f04220b1d835132e1ee96f19e3666b871655d587
shawn <shawn.yu5@hotmail.com>
2021-07-05 20:35:42 -0400

    Compile tee into file added
    -compile option now also pipes the output of compilation to both std out
    and a file named `output`, for debugging purposes

--- template-completion.bash
+++ template-completion.bash
@@ -1,3 +1,9 @@
 #/usr/bin/env bash
 
+#_template_completion()
+#{
+    #COMPREPLY+=($(ls *))
+    ##COMPREPLY+=("-compile debug -q")
+#}
 complete -W "-compile -debug -q" template
+#complete -F _template_completion template
